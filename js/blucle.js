8aa327b63e653c0871f06bc2f9462ed9989c26e5
Programador Novato <info@programadornovato.com>
2020-02-05 06:02:33 -0600

    Hacer un juego en Javascript🕹️ [06.- Implementar aps, fps y agregaremos un botón con su evento]
    Video:
    https://youtu.be/ci-WleK2M7Y

--- js/blucle.js
+++ js/blucle.js
@@ -5,14 +5,27 @@ var bucleInfinito={
     fps:0,//Frames por segundo
     bucle:function(tiempo){
         bucleInfinito.id=window.requestAnimationFrame(bucleInfinito.bucle);
+        bucleInfinito.actializar();
+        bucleInfinito.refrescar();
         var diferencia=tiempo-bucleInfinito.fin;
         if( diferencia > 999 ){
             console.log(`
             fin=${bucleInfinito.fin}\n
             tiempo=${tiempo}\n
             diferencia=${diferencia}\n
+            aps=${bucleInfinito.aps}\n
+            fps=${bucleInfinito.fps}\n
+            id=${bucleInfinito.id}\n
             `);
             bucleInfinito.fin=tiempo;
+            bucleInfinito.fps=0;
+            bucleInfinito.aps=0;
         }
+    },
+    actializar:function(){
+        bucleInfinito.aps++;
+    },
+    refrescar:function(){
+        bucleInfinito.fps++;
     }
 }
--- js/evento.js
+++ js/evento.js
@@ -1,4 +1,10 @@
+/*
 document.addEventListener('DOMContentLoaded',function(){
     console.log("Inicia juego");
+    bucleInfinito.bucle();
+});
+*/
+var start=document.getElementById("start");
+start.addEventListener('click',function(){
     bucleInfinito.bucle();
 });
