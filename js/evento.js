f9ed2a5d5a946e93567c37e76762999e67598431
Programador Novato <info@programadornovato.com>
2020-02-04 19:19:41 -0600

    Hacer un juego en Javascript🕹️ [04.- Eventos en Javascript]
    Video:
    https://youtu.be/NQNVAt79Wjc

---
+++ js/evento.js
@@ -0,0 +1,6 @@
+//console.error("Tonto te equivocaste");
+//console.warn("Casi la cag...");
+document.addEventListener('DOMContentLoaded',function(){
+    console.log("Suscribete con load");
+});
+console.log("Suscribete sin load");
