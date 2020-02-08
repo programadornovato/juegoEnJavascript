4e4f2aeae634c88cb54352bad0e8a5aeb9d93049
Programador Novato <info@programadornovato.com>
2020-02-08 00:29:25 -0600

    Hacer un juego en Javascript🕹️ [09.- Llenar de tiles (azulejos) nuestro documentos]
    Video:
    https://youtu.be/ySLIqzlEe0Q

--- js/evento.js
+++ js/evento.js
@@ -4,9 +4,13 @@ document.addEventListener('DOMContentLoaded',function(){
     var a1=new Area(0,30,100,100);
     var a2=new Area(100,30,100,100);
     medidas.tomaMedida();
+    medidas.dibujarTiles();
 });
 
 var start=document.getElementById("start");
 start.addEventListener('click',function(){
     bucleInfinito.bucle();
+});
+window.addEventListener('resize',function () {
+    medidas.dibujarTiles();
 });
--- js/medidas.js
+++ js/medidas.js
@@ -1,6 +1,8 @@
 var medidas={
     ancho:window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
     alto:window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+    tilesTam:100,
+    tilesEscala:1,//1=100% ejem: 0.50=50%
     tomaMedida:function(){
         window.addEventListener('resize',function(){
             medidas.ancho=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -11,5 +13,26 @@ var medidas={
 
             `);
         });
+    },
+    getNumTilesAncho:function () { 
+        var nuevaMedidaTiles=medidas.tilesTam*medidas.tilesEscala;
+        return Math.ceil((medidas.ancho-nuevaMedidaTiles)/nuevaMedidaTiles);
+        //772-100=672/100=ceil(6.72)=7
+    },
+    getNumTilesAlto:function () { 
+        var nuevaMedidaTiles=medidas.tilesTam*medidas.tilesEscala;
+        return Math.ceil((medidas.alto-nuevaMedidaTiles)/nuevaMedidaTiles);
+    },
+    getNumTilesTotal:function () {
+        return medidas.getNumTilesAncho*medidas.getNumTilesAlto;
+    },
+    dibujarTiles:function(){
+        var nuevaMedidaTiles=medidas.tilesTam*medidas.tilesEscala;
+        for (var y = 0; y < medidas.getNumTilesAlto(); y++) {
+            for (var x = 0; x < medidas.getNumTilesAncho(); x++) {
+                Area(x*nuevaMedidaTiles,y*nuevaMedidaTiles,nuevaMedidaTiles,nuevaMedidaTiles);
+            }
+        }
     }
+
 }
