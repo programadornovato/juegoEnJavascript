2d1a12d911365a818cd3065690fad1d50d5e9e31
Programador Novato <info@programadornovato.com>
2020-02-09 12:08:01 -0600

    Hacer un juego en Javascript🕹️ [12.- Crear nuestro mapa con tiled]
    Video:
    https://youtu.be/dWjeSfvXIgg

---
+++ img/blowharder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="blowharder" tilewidth="16" tileheight="16" tilecount="4408" columns="116">
+ <image source="blowharder.png" width="1856" height="608"/>
+</tileset>
