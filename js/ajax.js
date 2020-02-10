c1f3bff9ebf55abf7317cdd7ff07a1d94ec3daf2
Programador Novato <info@programadornovato.com>
2020-02-10 08:58:32 -0600

    Hacer un juego en Javascript🕹️ [13.-Llamar los datos de nuestro juego de forma asincronica con AJAX]
    Video:
    https://youtu.be/udSVlxKC_8c

---
+++ js/ajax.js
@@ -0,0 +1,13 @@
+var ajax={
+    carga:function (ruta) {
+        fetch(ruta)
+        .then(function (respusta) {
+            return respusta.text();
+        }).then(function (data) {
+            console.log(JSON.parse(data));
+        }).catch(function (err) {
+            console.error(err);
+        });
+    }
+}
+ajax.carga("img/juego.json");
